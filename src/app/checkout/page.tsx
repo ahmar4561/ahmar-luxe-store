fb89cc1d4e8906552cf5b570e2879c999c2a8c30
ahmar memon <ahmeralimemon187@gmail.com>
2026-03-08 01:07:44 +0500

    Re-triggering build for live site

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -5,5 +5,5 @@ export default function CheckoutPage() {
     <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-2xl font-bold">Checkout is processing...</h1>
     </div>
-  );
+  ); 
 }
